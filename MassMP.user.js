0f0d39a5d7ea7f1eb0b0c84ed7044a8a1077347e
Snizzle-jvc <badexicet@tempmailapp.com>
2019-07-08 12:14:50 +0200

    Update MassMP.user.js

--- MassMP.user.js
+++ MassMP.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        MassMP
 // @author      Snizzle
-// @version     3.0
+// @version     3.1
 // @downloadURL https://github.com/Snizzle-jvc/MassMP/raw/master/MassMP.user.js
 // @updateURL   https://github.com/Snizzle-jvc/MassMP/raw/master/MassMP.user.js
 // @supportURL  http://www.jeuxvideo.com/messages-prives/nouveau.php?all_dest=Snizzle;Snitchzzle
@@ -18,29 +18,27 @@ $(function(a) {
     function g() {
         a(".add-user").each(function() {
             var b = a(this).parent().parent().children().html().trim().toLowerCase();
-            localStorage.getItem("massmp").includes(b) && a(this).hide()
+            (localStorage.getItem("massmp").includes(b) || b == h) && a(this).hide()
         })
     }
     document.head.insertAdjacentHTML("beforeend", '<style type="text/css" id="massmp-css">\n.mmp-panel {\npadding: 7px;\nborder-bottom: 1px solid #ccc;\nmargin-bottom: 0.625rem;\n}\n\n.mmp-panel span:hover{\ncolor: #f00;\n}\n\n.mmp-compteur,.mmp-compteur:hover {\nfont-weight: 700;\ncolor:initial!important;\n}\n\n.mmp-btn,.mmp-btn:hover {\ncursor:pointer;\nbackground: #d7d7d7;\npadding: 2px 5px 2px 5px;\ncolor:initial!important;\nmargin-left: 5px;\n}\n\n.mmp-btn:active {\nbox-shadow: inset 0 0.1875rem 0.3125rem rgba(0,0,0,0.125);\n}\n\n.mmp-title {\nmargin-bottom: 10px;\n}\n\n.add-user {\nbackground: url(\'http://static.jvc.gg/1.96.4/img/profils/ico-pseudo-profil.png\') no-repeat -18px 0px;\ndisplay: inline-block;\nborder-radius: 3px;\nwidth: 1.3rem;\nheight: 1rem;\nfilter: invert(1) hue-rotate(147deg);\n}\n</style>');
-    var h = console.log;
+    var h = a(".account-pseudo").html().toLowerCase();
     null === localStorage.getItem("massmp") && localStorage.setItem("massmp", "[]");
     var e = JSON.parse(localStorage.getItem("massmp")) || [];
     a(".bloc-options-msg").append('<span class="add-user" title="Ajouter ce pseudo au MP" style=""></span>');
-    a(".bloc-outils-top").after('<div class="mmp-panel"><div class="mmp-title"><div class="mmp-liste"></div><span class="mmp-compteur">MassMP: ' + e.length + '/100 </span><span class="mmp-btn mmp-massadd pull-right" title="Ajouter la page actuelle dans le MP">Ajouter la page enti\u00e8re</span><span class="mmp-btn mmp-send pull-right">Envoyer MP</span><span class="mmp-btn mmp-clear pull-right icon-bin" title="Vider la liste"></span></div>');
+    a(".bloc-pre-pagi-forum.bloc-outils-top").after('<div class="mmp-panel"><div class="mmp-title"><div class="mmp-liste"></div><span class="mmp-compteur">MassMP: ' + e.length + '/100 </span><span class="mmp-btn mmp-massadd pull-right" title="Ajouter la page actuelle dans le MP">Ajouter la page enti\u00e8re</span><span class="mmp-btn mmp-send pull-right">Envoyer MP</span><span class="mmp-btn mmp-clear pull-right icon-bin" title="Vider la liste"></span></div>');
     0 == e.length && a(".mmp-liste").hide();
     g();
     (function() {
         for (var b = 0; b < e.length; b++) a(".mmp-panel").append("<span id='" + e[b].toLowerCase() + "' title='Supprimer ce pseudo du MP' style='cursor:pointer;'>" + e[b].toLowerCase() + "  </span>")
     })();
     a(".add-user").click(function() {
-        var b = (JSON.parse(localStorage.getItem("massmp")) || []).length;
+        var b = (JSON.parse(localStorage.getItem("massmp")) || []).length,
+            c = a(this).parent().parent().children().html().trim().toLowerCase();
         if (100 > b) {
-            var c = a(this).parent().parent().children().html().trim().toLowerCase();
-            h(c);
             var d = JSON.parse(localStorage.getItem("massmp")) || [];
             d.push(c);
-            localStorage.setItem("massmp",
-                JSON.stringify(d));
+            localStorage.setItem("massmp", JSON.stringify(d));
             a(this).hide();
             a(".mmp-panel").show();
             a(".mmp-panel").append("<span id='" + c.toLowerCase() + "' title='Supprimer ce pseudo du MP' style='cursor:pointer;'>" + c.toLowerCase() + "  </span>");
@@ -53,21 +51,21 @@ $(function(a) {
             c = b.length,
             d = a(this).attr("id");
         d = b.indexOf(d);
-        0 <= d && (b.splice(d, 1), a(this).hide(), localStorage.setItem("massmp",
-            JSON.stringify(b)), a(".mmp-compteur").text("MassMP: " + --c + "/100 "))
+        0 <= d && (b.splice(d, 1), a(this).hide(), localStorage.setItem("massmp", JSON.stringify(b)),
+            a(".mmp-compteur").text("MassMP: " + --c + "/100 "))
     });
     a(".mmp-massadd").click(function() {
         var b = (JSON.parse(localStorage.getItem("massmp")) || []).length;
         a(".add-user").each(function() {
             var c = a(this).parent().parent().children().html().trim().toLowerCase();
-            if (!localStorage.getItem("massmp").includes(c) && 100 > b) {
+            if (!localStorage.getItem("massmp").includes(c) && 100 > b && c != h) {
                 var d = JSON.parse(localStorage.getItem("massmp")) || [];
                 d.push(c);
                 localStorage.setItem("massmp", JSON.stringify(d));
                 a(this).hide();
                 a(".mmp-panel").show();
-                a(".mmp-panel").append("<span id='" +
-                    c.toLowerCase() + "' title='Supprimer ce pseudo du MP' style='cursor:pointer;'>" + c.toLowerCase() + "  </span>");
+                a(".mmp-panel").append("<span id='" + c.toLowerCase() +
+                    "' title='Supprimer ce pseudo du MP' style='cursor:pointer;'>" + c.toLowerCase() + "  </span>");
                 a(".mmp-compteur").text("MassMP: " + ++b + "/100 ");
                 g()
             }
